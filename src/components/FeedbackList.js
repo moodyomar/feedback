a51b9c2f69216eb1cec11b1e17b0dd45ea6a8752
moodyomar <lonak.secret@gmail.com>
2022-02-20 08:33:11 +0200

    add spinner on loading

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,28 +1,30 @@
-import {motion,AnimatePresence} from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem'
 import { useContext } from 'react';
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './common/Spinner';
 
-const FeedbackList = () => { 
-const {feedback} = useContext(FeedbackContext);
-  if(!feedback || feedback.length === 0){
+const FeedbackList = () => {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedbacks yet!</p>
   }
 
-  return(
-
-    <div className='feedback-list'>
+  return isLoading ? ( <Spinner/> ) 
+  :
+   (
+      <div className='feedback-list'>
       <AnimatePresence>
-    {feedback.map((item) => (
-      <motion.div 
-      key={item.id}
-      initial={{opacity:0}}
-      animate={{opacity:1}}
-      exit={{opacity:0}}>
-    <FeedbackItem key={item.id} item={item} />
-    </motion.div>
-    ))}
-    </AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}>
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
     
     )
